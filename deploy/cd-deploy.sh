160b7542fb3ca650dada6ef613cad40ab3a28d17
gnzjgo <gonzalojgo@gmail.com>
2024-01-08 22:34:01 +0100

    reqs.txt and semver to deploy.sh

--- deploy/cd-deploy.sh
+++ deploy/cd-deploy.sh
@@ -2,7 +2,5 @@
 source .tinyenv
 
 tb --semver ${VERSION} deploy --v3
-tb pipe populate tmp_move_unchanged_data
-tb pipe populate tmp_move_changed_data
-tb pipe rm tmp_move_unchanged_data
-tb pipe rm tmp_move_changed_data
+tb --semver ${VERSION} pipe populate tmp_move_unchanged_data
+tb --semver ${VERSION} pipe populate tmp_move_changed_data
