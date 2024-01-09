3a6944ae8160afe2bb8c573d8ef4f43f021cd5e3
gnzjgo <gonzalojgo@gmail.com>
2024-01-09 09:01:49 +0100

    version bump and add --node to pipe populate

--- deploy/cd-deploy.sh
+++ deploy/cd-deploy.sh
@@ -2,5 +2,5 @@
 source .tinyenv
 
 tb --semver ${VERSION} deploy --v3
-tb --semver ${VERSION} pipe populate tmp_move_unchanged_data
-tb --semver ${VERSION} pipe populate tmp_move_changed_data
+tb --semver ${VERSION} pipe populate tmp_move_unchanged_data --node events_old_to_new_unchanged --wait
+tb --semver ${VERSION} pipe populate tmp_move_changed_data --node events_old_to_new_changed --wait
